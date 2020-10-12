5d2513f723098595c4285388db1cc6fc39715425
ThisisPraveenDeshamoni <Deshamonepraveenkumar@hotmail.com>
2020-10-12 17:38:43 +0530

    cloud

---
+++ code.py
@@ -0,0 +1,10 @@
+import calendar
+
+yy = 2020  
+mm = 10   
+
+# we can also take input from user. below is the method to use
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+print(calendar.month(yy, mm))
