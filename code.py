3bac6625332c6ce9868e604212c0bd80999f24b0
ThisisPraveenDeshamoni <70310000+ThisisPraveenDeshamoni@users.noreply.github.com>
2020-10-12 18:33:53 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,4 @@
 import calendar
-
-yy = 2020  
-mm = 10   
-
-# we can also take input from user. below is the method to use
-# yy = int(input("Enter year: "))
-# mm = int(input("Enter month: "))
-
-print(calendar.month(yy, mm))
+c = calendar.TextCalendar(calendar.FRIDAY)
+str = c.formatmonth(2020, 10, 0, 0)
+print(str)
